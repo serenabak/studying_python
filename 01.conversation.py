0196e1d74e4f2f0bfcc4aecaf50b2dd0090259dd
Serena Bak <serena@Serenaui-MacBookPro.local>
2019-02-11 20:14:19 +0900

    Add 02.data-type-1.py
    - Learn int, float, str

---
+++ 01.conversation.py
@@ -0,0 +1,8 @@
+##화면에 데이터 출력하기
+# print(1+2)
+
+##사용자로부터 값 받기
+##변수 활용
+# name = input("what's your name?")
+# print("Hello! " + name)
+# print("안녕" + "하세요")
---
+++ 02.data-type-1.py
@@ -0,0 +1,11 @@
+# 정수 int - integer
+print(1 + 1)
+print(4 // 2)
+print(int(4/2))
+print(int("1"))
+# 소수 float
+print(1.5 + 2.1)
+print(float(2))
+# 문자 str - string
+print("Hello")
+print("1")
